92148f6946b016bf20aeef4f95062a58d1958e67
Filip Wästberg <filip.wastberg@gmail.com>
2017-02-20 11:46:58 +0100

    Use RCurl instead of curl to download the data set
    Had problems with the function, I suspect that it has to do with the fact that I'm working on a shared network. These changes omitted the problem.

--- RPackage/R/get_polls.R
+++ RPackage/R/get_polls.R
@@ -5,18 +5,12 @@
 #'
 #' @export
 get_polls <-function(){
-  # Create file path
-  file_tmp_path <- paste0(tempdir(), "/polls.csv")
+  # Get URL
+  x <- RCurl::getURL("https://raw.githubusercontent.com/MansMeg/SwedishPolls/master/Data/Polls.csv")
   
-  file_to_dl <- "https://raw.githubusercontent.com/MansMeg/SwedishPolls/master/Data/Polls.csv"   
+  # Read csv
+  df <- utils::read.csv(textConnection(x), stringsAsFactors = FALSE)
   
-  # Assert access to source
-  stopifnot(!httr::http_error(file_to_dl))
-    
-  # Download file
-  utils::download.file(file_to_dl, destfile = file_tmp_path, method = "curl", quiet = TRUE)
-
-  df <- utils::read.csv(file_tmp_path, stringsAsFactors = FALSE)
   df$Company <- factor(df$Company)
   df$PublDate <- lubridate::ymd(df$PublDate)
   df$collectPeriodFrom <- lubridate::ymd(df$collectPeriodFrom)
